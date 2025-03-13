aadc0f8e71aee24b9fa7a384b833c59c2542abb4
23f1002165 <23f1002165@ds.study.iitm.ac.in>
2025-03-13 06:06:04 +0000

    commit

--- backend/celery/celery_schedule.py
+++ backend/celery/celery_schedule.py
@@ -6,10 +6,10 @@ celery_app = app.extensions['celery']
 celery_app.conf.beat_schedule = {
     "send_service_reminders": {
         "task": "backend.celery.tasks.send_service_reminders",
-        "schedule": crontab(hour=9, minute=10),
+        "schedule": crontab(hour=18, minute=0),
     },
     "send_monthly_reports": {
         "task": "backend.celery.tasks.send_monthly_report",
-        "schedule": crontab(day_of_month=12, hour=9, minute=10),
+        "schedule": crontab(day_of_month=1, hour=18, minute=0),
     }
 }
